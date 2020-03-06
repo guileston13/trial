35f44167a0e1e164051382a3e62dc219a38713cc
Marix <marixawiten@gmail.com>
2020-03-06 21:40:59 +0800

    form9

--- application/views/update/teacher/form9.php
+++ application/views/update/teacher/form9.php
@@ -1,359 +1,352 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<style>
-			.letter{
-			 line-height: 0.3;
-			 text-align:center;
-			font-family:Arial;
-			}
-			.reportcard{
-						font-size: 40px;
-						text-align:center;
-			}
+<head>
+  <title>Report Card</title>
 
-			.parents{
-				padding-left:700px;
-				padding-top:10px;
-			}
-			.underline{
-				padding-left:1000px;
-				padding-top:100px;
-			
-			}
-			.underline2{
-				padding-left:600px;
-				padding-top:50px;
-			
-			}
-			.underline3{
-				padding-left:600px;
-				padding-top:100px;
-			
-			}
-			.underline4{
-				padding-left:1000px;
-				padding-top:-100px;
-				margin-top: -17;
-			}
-			.underline5{
-				padding-left:1000px;
-				padding-top:-100px;
-				margin-top: -17;
-			}
-			.teacher{
-				padding-left:1120px;
-				padding-top:5px;
-				font-size:20px;
-			
-			}
-			.teacher1{
-				padding-left:700px;
-				padding-top:1px;
-				font-size:20px;
-			
-			}
-				.principal2{
-				padding-left:700px;
-				padding-top:5px;
-				font-size:20px;
-			
-			}
-				.principal3{
-				padding-left: 1110px;
-				padding-top:-100px;
-				font-size:20px;
-				margin-top: -17;
-			
-			}
-				.certificate{
-					text-align:center;
-					font-size:25px;
-			
-			}
-				.certification{
-					text-align:center;
-					font-size:20px;
-					padding-right: 600px;
-			
-			}
-				.Date{
-					text-align:center;
-					font-size:20px;
-					padding-right: 440px;
-			
-			}
-				.approved{
-					text-align:center;
-					padding-right:450px;
-					font-size:20px;
-			
-			}
-				.principal4{
-				padding-left:100px;
-				font-size:20px;
-				    padding-left: 1200;
-				}
+    <link href="<?php echo base_url(); ?>/assets/img/brand/favicon.png" rel="icon" type="image/png">
+    <!-- Icons -->
+    <link href="<?php echo base_url(); ?>/assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <!-- Argon CSS -->
+    <link type="text/css" href="<?php echo base_url(); ?>/assets/css/argon.css?v=1.0.0" rel="stylesheet">
+   <link type="text/css" href="<?php echo base_url(); ?>/assets/css/custom_css.css" rel="stylesheet">
+   <style>
 
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+td, th{
+	border: 1px solid #1e4f8e;text-align: left;padding: 8px;
+}
 
-
-
-
-
-
-
-		</style>
-
-
-	</head>
+</style>
 	<body>
-		<strong><p><div class="letter">Republic of the Philippines</p></strong>
-		<strong><p>Department of Education</p></strong>
-		<p>Region 10</p>
-		<p>District of Misamis Oriental</p>
-		</div>
-		<br>
-		<center>_________________________________________________________</center>
-		<strong><center>District1</center></strong>
-		<center>_________________________________________________________</center>
-		<center><strong>School</strong></center>
-		<center>__________________________________________</center>
-		<center><strong>School Year</strong></center>
-			<br>
-			<br>
-			<br>
-				<div class="reportcard">Report Card</div>
-				<br>
-				<br>
-				<br>
-		<center><strong>Name:____________________________________________________________________________________</strong></center>
-		<center><strong>Age:___________________________________________  Sex: ______________________________________</strong></center>
-		<center><strong>Grade:_________________________________________ Section: _____________________________________</strong></center>
-		<br>
-		<br>
-		<strong><div class="parents">Dear Parents</p></strong></div>
-		<br>
-					<strong><center>This report card shows the ability and progress of your <br>child has made  in the different learning areas as well as his/her
-				    progress in character development.
-					<br>
-					This school welcomes you if you desire to know more
-					about the progress of your cost.</center></strong>
+		<div class="container-fluid" id="print_container" style="text-align: center;">
+
+			<div class="row">
+				
+				<div class="col-md-2" style="background-color: silver">
 					
-				<div class="underline">______________________________________</div>	
-				<div class="teacher">Teacher</div>	
-				<br>
-				<div class="underline2">__________________________________</div>
-				<div class="principal2">Principal</div>		
-				<br>
-							<div class="certificate">Certificate Of Transfer</div>
-					<center><strong><p>Admitted to Grade:________________________ Section:__________________________________
+				</div>
+			
+				<div class="col-md-8">
+					<b><h2>Republic of the Philippines</h2></b>		
+					<b><h2>Department of Education</h2></b>
 					<br>
-					Eligible for Administration to Grade: ______________________________________________</strong></center>
+					<center><input class="form-control-sm" type="text" name=""></center>
+						<label for="" class="form-control-label mr-2">District</label>
+					<center><input class="form-control-sm" type="text" name=""></center>
+						<label for="" class="form-control-label mr-2">School</label>
+					<center><input class="form-control-sm" type="text" name=""></center>
+						<label for="" class="form-control-label mr-2">School Year</label>
+					<div class="row">
+						<div class="col-md-6">
+							<b>Name:</b>
+							<input class="form-control-sm" type="text" name="">									
+						</div>
+					<br><br>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<b>Age:</b>
+							<input class="form-control-sm" type="text" name="">									
+						</div>
+						<div class="col-md-3">
+							<b>Sex:</b>
+							<input class="form-control-sm" type="text" name="">									
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-md-6">
+							<b>Grade:</b>
+							<input class="form-control-sm" type="text" name="">									
+						</div>
+						<div class="col-md-3">
+							<b>Section:</b>
+							<input class="form-control-sm" type="text" name="">									
+						</div>
+					<br><br>
+					</div>
 					<br>
 					<br>
-						<div class="approved">Approved:</div>
-						
-							<div class="underline3">__________________________________</div>    	<div class="underline4">__________________________________</div>
-		                    <div class="teacher1">Teacher</div>	  <div class="principal3">Principal</div>				
-											
-					<div class="certification">Certification of Eligibility to Transfer</div>
+					<b>Dear Parents</p></b>
 					<br>
-					<center><strong>Admitted in</strong>________________________________________________________________________________________________________</center>
-				<div class="Date">Date___________________________________________</div>    <div class="underline5">____________________________________________________</div>
-				<div class="principal4">Principal</div>
-				<strong><center>Periodic Rating</center></strong>
-				
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<center><table border="1"></center>
-			  <tr>
-				<th>Character Traits</th>
-				<th>1</th>
-				<th>2</th>
-				<th>3</th>
-				<th>4</th>
-			  </tr>
-			  <tr>
-				<td>1. Honesty</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>2. Courtesy</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			   <tr>
-				 <td>3. Helpfulness & Cooperation</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>  <tr>
-				 <td>4. Resourcefulness & Creativity</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td> 
-			  </tr>
-			  <tr>
-				 <td>5. Consideration for Others</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>6. Sportsmanship</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>7. Obedience</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>8. Self-Reliance</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>9. Industry</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>10. Cleanliness & Orderliness</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>11. Promptness & Punctuality</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>12. Sense of Responsibility</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>13. Love of God</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>14. Patience & Love of Country</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>Average</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-			  </tr>
-			  
-			</table>
-				<br>
-				<br>
-				<br>
-				<br>
-				<center><table border="1"></center>
-				
-				
-					<tr>
-				<th>Learning Areas</th>
-				<th>1</th>
-				<th>2</th>
-				<th>3</th>
-				<th>4</th>
-				<th>Final Rating</th>
-			  </tr>
-			  <tr>
-				<td>Filipino</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>English</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				 <td>January</td>
-			  </tr>
-			   <tr>
-				 <td>Mathematics</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				 <td>January</td>
-			  </tr>  <tr>
-				 <td>Makabayan</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>January</td>
-			  </tr>
-			  <tr>
-				 <td>Character Education</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				 <td>January</td>
-			  </tr>
-			  <tr>
-				 <td>Average</td>
-				<td>$100</td>
-				 <td>January</td>
-				<td>$100</td>
-				 <td>January</td>
-				 <td>January</td>
-			  </tr>
-				
-				</table>
-				<br>
-				<br>
-				<br>
-				<strong><p>PARENT GUARDIAN'S SIGNATURE</strong></p>
-				<p>1st Quarter ______________________________</p>
-				<p>2nd Quarter ______________________________</p>
-				<p>3rd Quarter ______________________________</p>
-				<p>4th Quarter ______________________________</p>
-		
+								<b><center>This report card shows the ability and progress of your <br>child has made  in the different learning areas as well as his/her
+							    progress in character development.
+								<br>
+								This school welcomes you if you desire to know more
+								about the progress of your cost.</center></b>
+								<br><br>
+								
+							<div class="row">
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">	
+																			
+										</div>
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">							
+										</div>
+										<br><br>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Principal</label>
+										</div>
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Teacher</label>
+										</div>	
+									</div>	
+							<br><br>
+										<h2>Certificate Of Transfer</h2>
+							<br><br>
+								<div class="row">
+									<div class="col-md-6">
+										<b>Admitted to Grade:</b>
+										<input class="form-control-sm" type="text" name="">									
+									</div>
+									<div class="col-md-3">
+										<b>Section:</b>
+										<input class="form-control-sm" type="text" name="">									
+									</div>
+								</div>
+								<br>
+								<div class="row">
+									<div class="col-md-7">
+										<b>Eligible for Administration to Grade:</b>
+										<input class="form-control-sm" type="text" name="">									
+									</div>
+								</div>
+									<h2>Approved:</h2>
+										<br><br>
+									<div class="row">
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">	
+																			
+										</div>
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">							
+										</div>
+										<br><br>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Principal</label>
+										</div>
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Teacher</label>
+										</div>	
+									</div>
+									<h2>Certification of Eligibility to Transfer:</h2>
+									<br>
+									<div class="row">
+										<div class="col-md-7">
+											<b>Admitted in</b>
+											<input class="form-control-sm" type="text" name="">									
+										</div>
+										<br><br>
+									</div>	
+									<div class="row">
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">	
+																			
+										</div>
+										<div class="col-md-6">
+											<input class="form-control-sm" type="text" name="">							
+										</div>
+										<br><br>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Date</label>
+										</div>
+										<div class="col-md-6">
+											<label for="" class="form-control-label mr-2">Principal</label>
+										</div>	
+									</div>
+							<br><br><br>
+							<b><center><h2>Periodic Rating</center></b></h2>
+							<br>
+							<br>
+							<table align="center">
+						  <tr>
+							<th >Character Traits</th>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+							<th>4</th>
+						  </tr>
+						  <tr>
+							<td>1. Honesty</td>
+							<td>a</td>
+							<td>b</td>
+							<td>c</td>
+							<td>d</td>
+						  </tr>
+						  <tr>
+							 <td>2. Courtesy</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						   <tr>
+							 <td>3. Helpfulness & Cooperation</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>  <tr>
+							 <td>4. Resourcefulness & Creativity</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td> 
+						  </tr>
+						  <tr>
+							 <td>5. Consideration for Others</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>6. Sportsmanship</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>7. Obedience</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>8. Self-Reliance</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>9. Industry</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>10. Cleanliness & Orderliness</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>11. Promptness & Punctuality</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>12. Sense of Responsibility</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>13. Love of God</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>14. Patience & Love of Country</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  <tr>
+							 <td>Average</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+						  </tr>
+						  
+						</table>
+							<br>
+							<br>
+							<br>
+							<br>
+							<table align="center">
+							
+							
+						<tr>
+							<th>Learning Areas</th>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+							<th>4</th>
+							<th>Final Rating</th>
+						</tr>
+						<tr>
+							<td>Filipino</td>
+							<td>a</td>
+							<td>b</td>
+							<td>a</td>
+							<td>b</td>
+							<td>b</td>
+						</tr>
+						<tr>
+							 <td>English</td>
+							 <td>a</td>
+							 <td>b</td>
+							 <td>c</td>
+							 <td>d</td>
+							 <td>b</td>
+						  </tr>						
+							</table>
+							<br>
+							<br>
+							<br>
+							<div class="row">
+								<div class="col-md-12">
+									<h3>PARENT GUARDIAN'S SIGNATURE</h3>
+								</div>
+								
+							</div>
+							<br><br>
+							<div class="row">
+								<div class="col-md-10">1st Quarter ________________</div>
+								<br><br>
+							</div>
+							<div class="row">
+								<div class="col-md-10">2nd Quarter ________________</div>
+								<br><br>
+							</div>
+							<div class="row">
+								<div class="col-md-10">3rd Quarter ________________</div>
+								<br><br>
+							</div>
+							<div class="row">
+								<div class="col-md-10">4th Quarter ________________</div>
+								<br><br>
+							</div>
+
+							<br><br><br>
+				</div>
+
+				<div class="col-md-2" style="background-color: silver">
+				</div>
+			</div>
+		</div>
 </body>
 </html>
